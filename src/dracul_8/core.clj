791533364847c0f80cb08dc1f3ffa0a1bc55f034
Markus Flambard <markus@flambard.se>
2021-12-04 18:48:32 +0100

    Prepare for using command line argument for ROM file path

--- src/dracul_8/core.clj
+++ src/dracul_8/core.clj
@@ -9,16 +9,20 @@
 (defn -main
   "Start the GUI"
   [& args]
-  (let [screen-canvas (canvas :id :canvas
-                              :background "#222222"
-                              :paint nil)
-        frame (frame :title "DRACUL-8"
-                     :width 512
-                     :height 256
-                     :on-close :exit
-                     :content screen-canvas)
-        beep (square-wave 0.5 110)]
-    (listen frame :key-pressed (fn [key]
-                                 (play beep 44100)
-                                 (println (.toString key))))
-    (show! frame)))
+  (if (nil? args)
+    (println "Please provide a path to a ROM file.")
+    (let [file-path (first args)
+          screen-canvas (canvas :id :canvas
+                                :background "#222222"
+                                :paint nil)
+          frame (frame :title "DRACUL-8"
+                       :width 512
+                       :height 256
+                       :on-close :exit
+                       :content screen-canvas)
+          beep (square-wave 0.5 110.0)]
+      (println "Running ROM:" file-path)
+      (listen frame :key-pressed (fn [key]
+                                   (play beep 44100)
+                                   (println (.toString key))))
+      (show! frame))))
